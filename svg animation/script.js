dde9379f9b652b7437912abf19d0e8fb776c4b7d
Muhammad Noman <nomanofficial497@gmail.com>
2025-10-04 11:38:06 +0500

    Svg animation added & learned

---
+++ svg animation/script.js
@@ -0,0 +1,26 @@
+
+
+var path = `M 41 150 Q 250 150 1310 150`
+
+var finalPath = `M 41 150 Q 250 150 1310 150`
+
+
+const string = document.querySelector(".string");
+
+string.addEventListener("mousemove", function (dets) {
+    path = `M 41 150 Q ${dets.x} ${dets.y} 1310 150`;
+
+    gsap.to("svg path", {
+        attr: { d: path },
+        duration: 0.2,
+        ease: "power3.out",
+    })
+})
+
+string.addEventListener("mouseleave", (dets) => {
+    gsap.to("svg path", {
+        attr: { d: finalPath },
+        duration: 0.9,
+        ease: "elastic.out(1,0.25)",
+    })
+})
